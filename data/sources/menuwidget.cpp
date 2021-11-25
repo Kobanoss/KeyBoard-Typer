90e48198c26fff6c290364cc04f38d6cc7a98798
Kobanoss <47609905+Kobanoss@users.noreply.github.com>
2021-11-25 17:24:23 +0300

    Add files via upload

--- data/sources/menuwidget.cpp
+++ data/sources/menuwidget.cpp
@@ -3,9 +3,32 @@
 
 MenuWidget::MenuWidget(QWidget *parent): QWidget(parent), ui(new Ui::MenuWidget) {
     ui->setupUi(this);
+
 }
 
 MenuWidget::~MenuWidget() {
     delete ui;
+}
+
+
+void MenuWidget::on_pushButton_main_start_clicked() {
+    AnotherWindow = new InputWidget();
+
+    connect(AnotherWindow, &InputWidget::changeWindow, this, &MenuWidget::show);
+    connect(this, &MenuWidget::sendValues,AnotherWindow, &InputWidget::setValues);
+    this->lang = ui->radioButton_ru->isChecked()?"ru":"";
+    this->lang += ui->radioButton_en->isChecked()?"en":"" ;
+    this->lang += ui->radioButton_spec->isChecked()?"spec":"";
+
+    this->type = ui->comboBox_main_type->currentText();
+
+    this->execution_time = ui->spinBox_main_time->text();
+    qDebug() << "wanna see " << execution_time;
+
+    (ui->lineEdit_main_username->text() == "")?this->username="default":this->username = ui->lineEdit_main_username->text();
+
+    AnotherWindow->show();
+    emit sendValues(username, lang, type, execution_time);
+    this->close();
 }
 
